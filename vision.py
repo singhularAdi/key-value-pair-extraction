0da66a14927b0aa49b2671db610dd5526593f97f
Aditya Pratap Singh <adi18jan1999@gmail.com>
2020-11-23 20:09:03 +0530

    added calls to gcp for vision api

---
+++ vision.py
@@ -0,0 +1,28 @@
+import sys
+from bounding_utils import organise_data
+
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+
+    organise_data(texts, path)
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+
+image_path = sys.argv[1]
+detect_text(image_path)
